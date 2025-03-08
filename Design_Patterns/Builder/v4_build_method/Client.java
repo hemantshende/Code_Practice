a76f62e164c5f05e9e2b730b227025158c874e00
Hemant Shende <shendehc95@gmail.com>
2025-03-08 16:08:41 +0530

    added build() inside StudentBuilder class

---
+++ Design_Patterns/Builder/v4_build_method/Client.java
@@ -0,0 +1,18 @@
+package Design_Patterns.Builder.v4_build_method;
+
+public class Client {
+    public static void main(String[] args) {
+
+//        StudentBuilder studentBuilder=new StudentBuilder();
+        Student student=
+                Student.getBuilder()
+                .setAge(25)
+                .setfName("abc")
+                .setlName("xyz")
+                .setEmail("abc@xyz").build();
+
+//        Student student=new Student(studentBuilder);
+
+        System.out.println(student.toString());
+    }
+}
